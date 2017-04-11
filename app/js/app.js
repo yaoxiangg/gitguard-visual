a8a5020b663fa07c57b5d93b39c5f1b022dd6426
Yao Xiang <teryaoxiang@gmail.com>
2017-04-11 22:13:47 +0800

    Add percentage to pie chart tooltip

--- app/js/app.js
+++ app/js/app.js
@@ -29,6 +29,21 @@ var app = angular.module('GitGuardApp', [
     ChartJsProvider.setOptions('pie', {
       scales: {
         display: false
+      },
+      tooltips: {
+        callbacks: {
+          label: function(tooltipItem, data) {
+            var allData = data.datasets[tooltipItem.datasetIndex].data;
+            var tooltipLabel = data.labels[tooltipItem.index];
+            var tooltipData = allData[tooltipItem.index];
+            var total = 0;
+            for (var i in allData) {
+              total += allData[i];
+            }
+            var tooltipPercentage = Math.round((tooltipData / total) * 100);
+            return tooltipLabel + ': ' + tooltipData + ' (' + tooltipPercentage + '%)';
+          }
+        }
       }
     });
 }]);
